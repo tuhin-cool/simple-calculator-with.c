4b3d7d86b0326ba60dda58cdaa1d0648960b5973
KomCat23 <artemkurnasenko2010@gmail.com>
2026-05-22 20:16:46 +0300

    Update simple-calculator-with.c
    A great project for a beginner. Just don't forget to compile this file.

--- simple-calculator-with.c
+++ simple-calculator-with.c
@@ -1,42 +1,39 @@
 #include<stdio.h>
+#include<math.h>
+
 int main(){
-    double num1, num2;
-    char oper;
-
-     printf("Enter an operator (+,-,*,/) =");
-    scanf("%c",&oper);
-
-
-    printf("Enter frist  numbers = ");
+	double num1, num2;
+	long results;
+    char oper[3];
+    
+	printf("Enter first  numbers = ");
     scanf(" %lf", &num1);
-
-    printf("Enter secund  numbers = ");
+	
+    printf("Enter second  numbers = ");
     scanf(" %lf", &num2);
+	
+	printf("Enter an operator (+,-,*,/,**) = ");
+    scanf("%s",&oper);
 
-    switch(oper)
-    {
-        case '+' :
-        {
-            printf("%.lf + %.lf = %.lf\n", num1, num2,num1+num2);
-            break;
-        }
-        case '-' :{
-            printf("%.lf + %.lf = %.lf\n", num1, num2,num1-num2);
-            break;
-        }
-         case '/' :
-         {
-            printf("%.lf + %.lf = %.lf\n", num1, num2,num1/num2);
-            break;
-        }
-        case '*' :
-        {
-            printf("%.lf + %.lf = %.lf\n", num1, num2,num1*num2);
-            break;
-        }
-        default:
-        printf("Not a valid operator\n");
-
-     }
-      return 0;
+	if(strcmp(oper, "+") == 0){                   //I've rewritten the check to if else because it's better than the switch case and added more operators from himself
+		results=num1+num2;
+		printf("the value=%ld",results);
+    }
+    else if(strcmp(oper, "-") == 0){
+   		results=num1-num2;
+		printf("the value=%ld",results);
+    }
+    else if(strcmp(oper, "*") == 0){
+   		results=num1*num2;
+		printf("the value=%ld",results);
+    }
+    else if(strcmp(oper, "/") == 0){
+   		double results=num1/num2;
+		printf("the value=%lf",results);
+    }
+	else if(strcmp(oper, "**") == 0){
+   		results=pow(num1,num2);
+		printf("the value=%ld",results);
+    }
+    return 0;
  }
